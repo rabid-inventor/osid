7ef48104815ca1a4ad9aede699d4a58c0bf823de
Gee Bartlett <geeus81@gmail.com>
2014-03-19 16:49:29 +0000

    changed dcfldd call to work will rasbian

--- system/write.sh
+++ system/write.sh
@@ -22,7 +22,7 @@ then
    eval "$umountlist"
 
    #run the dcfldd command passing the imagefile and device list
-   eval "/usr/bin/dcfldd bs=4M if=/etc/osid/imgroot/$imagefile $devicelist sizeprobe=if statusinterval=1 2>&1 | tee /etc/osid/system/progress.info"
+   eval "sudo dcfldd bs=4M if=/etc/osid/imgroot/$imagefile $devicelist sizeprobe=if statusinterval=1 2>&1 | tee /etc/osid/system/progress.info"
    
    #wait 5 seconds after dcfldd command has finished to ensure php script has a chance to read progress.info
    sleep 5
